9c56fff10091585143a05fdec961726206d4c3b4
amit-kumar9766 <amitbalharakr93@gmail.com>
2020-10-05 23:38:32 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import Sidebar from './components/Sidebar';
 import LeftSide from './components/LeftSide';
 import Rightside from './components/Rightside';
 import Welcome from './components/Welcome'
+import {Link,BrowserRouter} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       {/* <Welcome/> */}
+      <Route path="/assignmentNew" exact>
       <Sidebar/>
       <LeftSide/>
       <Rightside/>
+   
+     </Route>
+    
     </div>
   );
 }
