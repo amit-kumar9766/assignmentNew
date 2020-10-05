92049d1aab1939a6c3e1b2c5570b64f6e7a5ee2e
amit-kumar9766 <amitbalharakr93@gmail.com>
2020-10-05 23:18:00 +0530

    Updates

---
+++ precache-manifest.c99b710fb249d41067fd3626844f7178.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01b0afa4add3b93f2f68f960f178be78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd96a198fbe8dcebcd0b",
+    "url": "/static/css/main.5f050224.chunk.css"
+  },
+  {
+    "revision": "53c20ba7a42dac3da6c4",
+    "url": "/static/js/2.daa35641.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.daa35641.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd96a198fbe8dcebcd0b",
+    "url": "/static/js/main.f0b7ef35.chunk.js"
+  },
+  {
+    "revision": "691c052bd89a0bf9f288",
+    "url": "/static/js/runtime-main.928b11e5.js"
+  },
+  {
+    "revision": "143eeac2b6a0b80935d331a2e514e5ae",
+    "url": "/static/media/boy.143eeac2.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
