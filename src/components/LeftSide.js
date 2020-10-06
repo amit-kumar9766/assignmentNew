b3239c896c1d8fb37e8debe0c9b29ecd4ec6d460
amit-kumar9766 <amitbalharakr93@gmail.com>
2020-10-06 15:45:39 +0530

    update

--- src/components/LeftSide.js
+++ src/components/LeftSide.js
@@ -26,29 +26,79 @@ const Main=()=>{
            </div>
       
         </div>
+      
 
-        {/* <div className="courses">Courses</div> */}
+      
+        
 
-        {/* <div className='courses__1'>
-            
+     <div className="filter-area">
+      
+     <h2 className="filters-title" style={{marginTop: "1.5em"}}>courses </h2>
+<div className="filter-tabs-area">
+    <a className="filters" href="#">All Courses</a>
+<a className="filters" href="#">The Newest</a>
+<a className="filters" href="#">Top Rated</a>
+<a className="filters" href="#">Most Popular</a>
+</div>
+    </div>
+
+<div className="courses-area">
+
+<div className="upper__body1">
+           <div >
+               <img src={require('./assets/course1.png')}/>
+           </div>
+           <div> 
+               Amit Kumar 
+           </div>
+           <div >
+              <Button  style={{width:'100px',height:'30px',backgroundColor:'black',borderRadius:'10px',color:'white'}}>Continue</Button>
+           </div>
+      
         </div>
 
-        <div className='courses__1'>
-            
+
+        <div className="upper__body1">
+           <div >
+               <img src={require('./assets/course1.png')}/>
+           </div>
+           <div> 
+               Amit Kumar 
+           </div>
+           <div >
+              <Button  style={{width:'100px',height:'30px',backgroundColor:'black',borderRadius:'10px',color:'white'}}>Continue</Button>
+           </div>
+      
         </div>
 
-        <div className='courses__1'>
-            
+
+        <div className="upper__body1">
+           <div >
+               <img src={require('./assets/course1.png')}/>
+           </div>
+           <div> 
+               Amit Kumar 
+           </div>
+           <div >
+              <Button  style={{width:'100px',height:'30px',backgroundColor:'black',borderRadius:'10px',color:'white'}}>Continue</Button>
+           </div>
+      
         </div>
 
-        <div className='courses__1'>
-            
+        <div className="upper__body1">
+           <div >
+               <img src={require('./assets/course1.png')}/>
+           </div>
+           <div> 
+               Amit Kumar 
+           </div>
+           <div >
+              <Button  style={{width:'100px',height:'30px',backgroundColor:'black',borderRadius:'10px',color:'white'}}>Continue</Button>
+           </div>
+      
         </div>
 
-        <div className='courses__1'>
-            
-        </div> */}
-
+</div>
 
 
 
